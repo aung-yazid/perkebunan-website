8925839ce6ccbdc297a4617c63e680b5e15f6ac7
aung-yazid <khaerulaung@gmail.com>
2024-08-16 13:58:17 +0700

    Project

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import DesktopLogo from "../../public/plantation.jpg";
+import MobileLogo from "../../public/plantation.jpg";
+import { UserNav } from "./UserNav";
+import { SearchModalCompnent } from "./SearchComponents";
+
+export function Navbar() {
+  return (
+    <nav className="w-full border-b">
+      <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
+        <Link href="/">
+          <Image
+            src={DesktopLogo}
+            alt="Desktop Logo"
+            className="w-32 hidden lg:block"
+          />
+
+          <Image
+            src={MobileLogo}
+            alt="Mobile Logo"
+            className="block lg:hidden w-12"
+          />
+        </Link>
+
+        <SearchModalCompnent />
+
+        <UserNav />
+      </div>
+    </nav>
+  );
+}
